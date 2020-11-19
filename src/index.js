65df8254310ccabeb832d3e36d2a9c4bed9904c6
Dan Connolly <dckc@madmode.com>
2020-11-18 18:56:42 -0600

    wallet CRUD, locking

---
+++ src/index.js
@@ -0,0 +1,113 @@
+// @ts-check
+const { freeze, keys, values, entries, fromEntries } = Object;
+
+function assert(cond) {
+  if (!cond) throw new Error();
+}
+
+/**
+ *
+ * @param {Record<string, V>} obj
+ * @param {(prop: string) => boolean} pred
+ * @template V
+ */
+function filterKeys(obj, pred) {
+  return fromEntries(entries(obj).filter(([prop, _]) => pred(prop)));
+}
+
+/**
+ *
+ * @param {Contents} initClear
+ * @param {Contents=} locked
+ */
+function WalletImpl(initClear, locked = {}) {
+  /** @type {Contents} */
+  let clear = initClear;
+
+  const self = freeze({
+    /**
+     * @param {Contents} byAddr
+     */
+    insert(byAddr) {
+      keys(byAddr).forEach((addr) => {
+        assert(!keys(clear).includes(addr));
+        assert(!keys(locked).includes(addr));
+      });
+      const update = { ...clear, ...byAddr };
+      assert(
+        [...values(clear), ...values(locked)].filter(([k, _]) => k === 'owner')
+          .length === 1,
+      );
+      clear = update;
+    },
+    getKeys() {
+      return [...keys(locked), ...keys(clear)];
+    },
+    /**
+     * @param {REVAddress} addr
+     */
+    getValue(addr) {
+      return locked[addr] || clear[addr];
+    },
+    /**
+     * @param {REVAddress[]} addrs
+     */
+    getValues(addrs) {
+      return fromEntries(addrs.map((addr) => [addr, self.getValue(addr)]));
+    },
+    /**
+     * @param {Contents} byAddr
+     */
+    update(byAddr) {
+      entries(byAddr).forEach(([addr, val]) => {
+        if (keys(clear).includes(addr)) {
+          clear[addr] = val;
+        } else if (keys(locked).includes(addr)) {
+          locked[addr] = val;
+        } else {
+          // ISSUE: partial updates?
+          throw new RangeError(addr);
+        }
+      });
+    },
+    /**
+     * @param {REVAddress[]} addrs
+     */
+    delete(addrs) {
+      addrs.forEach((addr) => {
+        assert(addr in clear || addr in locked);
+        delete clear[addr];
+        delete locked[addr];
+      });
+    },
+    /**
+     * @param {REVAddress[]} addrs
+     */
+    lock(addrs) {
+      const newLocked = filterKeys(clear, (addr) => addrs.includes(addr));
+      const newClear = filterKeys(clear, (addr) => !addrs.includes(addr));
+      return WalletImpl(newClear, newLocked);
+    },
+  });
+
+  return self;
+}
+
+/**
+ * Digital Accreditation wallet
+ *
+ * ISSUE: principal REVAddress is REVAddress of
+ * deployerId public key.
+ *
+ * @param {REVAddress} owner
+ * @param {Asset} idVerification
+ *
+ * ISSUE: Uint8array?
+ * @typedef {string} REVAddress
+ * @typedef {string} HumanFriendlyKey
+ * @typedef {unknown} Asset
+ * @typedef {Record<REVAddress, [HumanFriendlyKey, Asset]>} Contents
+ */
+export function Wallet(owner, idVerification) {
+  return WalletImpl({ [owner]: ['owner', idVerification] });
+}
--- test/test-hello.js
+++ test/test-hello.js
@@ -1,5 +1,46 @@
+// @ts-check
 import test from 'ava';
 
+import { Wallet } from '../src';
+
 test('hello', (t) => {
-  t.is(1 + 1, 2);
+  const acct1 = {
+    privateKey:
+      '56c693042a32b751c283cdc34e6a6747679be7b6ff8f3a207f51089b871e2ca2',
+    publicKey:
+      '045faa353676faaa03dab328ff2759f36a2a2cfefef1a28a5d97eb7ef471016e22d5c136f4940ee23b0c4d5df2a6870f64b68e9b907c2c69813ab5537072668285',
+    ETH: '19da96487e59a3dc4b37daebfc71362e310376bf',
+    REV: '11112FQqtitKJkBrTDpGo91UBTDbj82TX5iigCc3igLXfL6xtCaEX8',
+  };
+
+  const w1 = Wallet(acct1.REV, 'idcard1');
+  t.is(w1, w1);
+  w1.insert({ 111234: ['height', '6\'1"'], 111231: ['weight', '200lb'] });
+  const lw1 = w1.lock([acct1.REV]);
+  lw1.update({ [acct1.REV]: ['owner', 'idcard2'] });
+
+  t.deepEqual(w1.getValue(acct1.REV), ['owner', 'idcard1']);
+  t.deepEqual(lw1.getValue(acct1.REV), ['owner', 'idcard2']);
+
+  t.deepEqual(lw1.getKeys(), [
+    '11112FQqtitKJkBrTDpGo91UBTDbj82TX5iigCc3igLXfL6xtCaEX8',
+    '111231',
+    '111234',
+  ]);
+
+  t.deepEqual(
+    lw1.getValues([
+      '11112FQqtitKJkBrTDpGo91UBTDbj82TX5iigCc3igLXfL6xtCaEX8',
+      '111231',
+      '111234',
+    ]),
+    {
+      '11112FQqtitKJkBrTDpGo91UBTDbj82TX5iigCc3igLXfL6xtCaEX8': [
+        'owner',
+        'idcard2',
+      ],
+      111231: ['weight', '200lb'],
+      111234: ['height', '6\'1"'],
+    },
+  );
 });
